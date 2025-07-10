b10aad683c5c9ecbd45d0dac7afda80f330f83e5
Scott Windsor <swindsor@gmail.com>
2025-07-09 21:11:35 -0700

    use wrapProgram

--- hello-nix/default.nix
+++ hello-nix/default.nix
@@ -10,11 +10,7 @@ pkgs.rustPlatform.buildRustPackage {
   nativeBuildInputs = [ pkgs.makeWrapper ];
 
   postInstall = ''
-    # Move the original binary to a new location
-    mv $out/bin/hello-nix $out/bin/.hello-nix-unwrapped
-
-    # Create a wrapper script
-    makeWrapper $out/bin/.hello-nix-unwrapped $out/bin/hello-nix \
+    wrapProgram $out/bin/hello-nix \
       --prefix PATH : ${pkgs.lolcat}/bin \
       --prefix PATH : ${pkgs.figlet}/bin \
       --add-flags "| figlet | lolcat"
